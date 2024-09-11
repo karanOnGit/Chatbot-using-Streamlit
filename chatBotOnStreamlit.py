a9101e35e0c58b6a6410be5832d050735039d57e
Karan Bhardwaj <147686366+karanOnGit@users.noreply.github.com>
2024-09-11 19:11:00 +0530

    updated

--- chatBotOnStreamlit.py
+++ chatBotOnStreamlit.py
@@ -1,19 +1,19 @@
 import os
-
 import time as t
 from time import gmtime, strftime
 import streamlit as st
 import google.generativeai as genai
-from dotenv import find_dotenv, load_dotenv, dotenv_values
+from dotenv import load_dotenv
 
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
-api_key = os.environ["GOOGLE_API_KEY"]
+# dotenv_path = find_dotenv()
+load_dotenv()
+# api_key = os.environ["GOOGLE_API_KEY"]
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-
-model = genai.GenerativeModel("gemini-pro")
 chat = model.start_chat(history=[])
+
 
 def greetings():
     greet = t.localtime()
@@ -22,39 +22,49 @@ def greetings():
     day = strftime("%a", gmtime())
     date = current_time.tm_mday
     hour = greet.tm_hour
+    # st.header(day)
 
-    if 12 <= hour <= 18:
-        st.header(f":red[{day} {date}], Good Afternoon Karan")
-    elif 19 <= hour <= 23:
-        st.header(f":red[{day} {date}], Good Evening Karan")
-    else:
-        st.header(f":red[{day} {date}], Good Morning Karan")
+    if 6 <= hour <= 12:
+        st.header(f":red[{day} {date}], Good Morning! Karan")
+    elif 13 <= hour <= 18:
+        st.header(f":red[{day} {date}], Good Afternoon! Karan")
+    elif 19 <= hour <= 24:
+        st.header(f":red[{day} {date}], Good Evening! Karan")
+
 
 def get_gemini_response(question):
     response = genai.chat(messages=question)
     return response.last
 
 
-if __name__ == '__main__':
+def translate_role_for_streamlit(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+
+
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = model.start_chat(history=[])
+
+
+if __name__ == "__main__":
     greetings()
 
-    input = st.text_input(":boy: You: ")
-    # submit = st.button(":green[Ask]")
+    # Sending my message to chatBot and get the response
+    user_prompt = st.chat_input("Asking...")
+    if user_prompt:
+        st.chat_message("user").markdown(user_prompt)
 
-    if "chat_history" not in st.session_state:
-        st.session_state["chat_history"] = []
+        gemini_response = st.session_state.chat_session.send_message(user_prompt)
 
-    if input:
-        response = get_gemini_response(input)
-        st.session_state["chat_history"].append((":black[You]", input))
-        st.subheader(":robot_face: Bot reply: ", divider="rainbow")
-        st.write(response)
-        st.session_state["chat_history"].append((":black[Bot reply]", response))
+        # Displaying response
+        with st.chat_message("assistant"):
+            st.markdown(gemini_response.text)
 
-    # st.subheader("Chat History", divider="grey")
-    st.sidebar.header(":blue[Chat History]", divider="grey")
-    # st.sidebar.text("Your chats are saved here")
-    
-    for role, text in st.session_state["chat_history"]:
-        st.sidebar.write(f"**{role}**:  {text}")
-        st.sidebar.divider()
+    # if input:
+    #     response = get_gemini_response(input)
+    #     st.session_state["chat_history"].append((":black[You]", input))
+    #     st.subheader(":robot_face: Bot reply: ", divider="rainbow")
+    #     st.write(response)
+    #     st.session_state["chat_history"].append((":black[Bot reply]", response))
